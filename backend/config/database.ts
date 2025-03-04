7dd2fb4a8e55d2b87662ee51843cf039353ceb5d
kodaa <je.elbaze@gmail.com>
2025-03-04 15:46:25 +0100

    init, add neon db

---
+++ backend/config/database.ts
@@ -0,0 +1,18 @@
+import env from '#start/env'
+import { defineConfig } from '@adonisjs/lucid'
+
+const dbConfig = defineConfig({
+  connection: 'postgres',
+  connections: {
+    postgres: {
+      client: 'pg',
+      connection: env.get('DATABASE_URL'),
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+    },
+  },
+})
+
+export default dbConfig
