c5cf9a2dfdeb27a228fb865488fee36af890b92b
kodaa <je.elbaze@gmail.com>
2025-03-04 16:44:49 +0100

    do auth method with token

---
+++ backend/app/controllers/auth_controller.ts
@@ -0,0 +1,28 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+
+export default class AuthController {
+  async register({ request }: HttpContext) {
+    const data = request.only(['email', 'password', 'fullName'])
+    const user = await User.create(data)
+
+    return User.accessTokens.create(user)
+  }
+
+  async login({ request }: HttpContext) {
+    const { email, password } = request.only(['email', 'password'])
+    const user = await User.verifyCredentials(email, password)
+
+    return User.accessTokens.create(user)
+  }
+
+  async logout({ auth }: HttpContext) {
+    const user = auth.user!
+    await User.accessTokens.delete(user, user.currentAccessToken.identifier)
+  }
+
+  async me({ auth }: HttpContext) {
+    await auth.check()
+    return { user: auth.user }
+  }
+}
--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -1,16 +1,12 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+const AuthController = () => import('#controllers/auth_controller')
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router
+  .group(() => {
+    router.post('/register', [AuthController, 'register'])
+    router.post('/login', [AuthController, 'login'])
+    router.delete('/logout', [AuthController, 'logout']).use(middleware.auth())
+    router.get('/me', [AuthController, 'me'])
+  })
+  .prefix('/api')
